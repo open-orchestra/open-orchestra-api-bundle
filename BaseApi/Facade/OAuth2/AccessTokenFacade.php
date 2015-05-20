e3fba0ac37c42acbdce8fada8a2a9539824e5ed3
Nicolas Thal <nico.th4l@gmail.com>
2015-05-20 10:45:19 +0200

    change facade parameters

--- BaseApi/Facade/OAuth2/AccessTokenFacade.php
+++ BaseApi/Facade/OAuth2/AccessTokenFacade.php
@@ -17,6 +17,13 @@ class AccessTokenFacade extends AbstractFacade
 
     /**
      * @Serializer\Type("DateTime<'d-m-Y H:i:s'>")
+     *
+     * @deprecated use expiresAt instead, will be removed in 0.2.4
      */
     public $expiresIn;
+
+    /**
+     * @Serializer\Type("DateTime<'d-m-Y H:i:s'>")
+     */
+    public $expiresAt;
 }
