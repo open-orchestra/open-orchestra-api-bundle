e57bea550c91c5cf820f681429f644f416779b0c
Nicolas ANNE <nicolas.anne@businessdecision.com>
2015-05-27 17:00:11 +0200

    first full functionnal commit

--- BaseApiBundle/Controller/BaseController.php
+++ BaseApiBundle/Controller/BaseController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\EventDispatcher\Event;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 /**
  * Class BaseController
@@ -37,11 +38,11 @@ abstract class BaseController extends Controller
     }
 
     /**
-     * @param Request $request
-     * @param string $id
-     * @param string $type
-     * @param string $event
-     * @param string $eventClass
+     * @param Request                       $request
+     * @param string                        $id
+     * @param string                        $type
+     * @param string                        $event
+     * @param string                        $eventClass
      *
      * @return Response
      */
@@ -56,9 +57,17 @@ abstract class BaseController extends Controller
         );
 
         $mixed = $this->get('open_orchestra_model.repository.' . $typeName)->find($id);
+        $fromStatus = $mixed->getStatus();
         $mixed = $this->get('open_orchestra_api.transformer_manager')->get($typeName)->reverseTransform($facade, $mixed);
+        $toStatus = $mixed->getStatus();
 
-        if ($this->isValid($mixed)) {
+        $role = $this->get('open_orchestra_model.repository.role')->findOneByFromStatusAndToStatus($fromStatus, $toStatus);
+        $workflowFunctions = $this->get('open_orchestra_workflow_function.repository.workflow_function')->findByRole($role);
+        $attributes = array();
+        foreach($workflowFunctions as $workflowFunction){
+            $attributes[] = $workflowFunction->getId();
+        }
+        if ($this->isValid($mixed) && $this->get('security.authorization_checker')->isGranted($attributes, $mixed)) {
             $em = $this->get('doctrine.odm.mongodb.document_manager');
             $em->persist($mixed);
             $em->flush();
