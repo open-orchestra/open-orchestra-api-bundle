691d4c12c4012e681efcc62ff4c7c9d724bcffcd
Nicolas Thal <nico.th4l@gmail.com>
2015-06-03 11:52:11 +0200

    add parametrable expiration time for token

--- BaseApiBundle/DependencyInjection/OpenOrchestraBaseApiExtension.php
+++ BaseApiBundle/DependencyInjection/OpenOrchestraBaseApiExtension.php
@@ -42,6 +42,7 @@ class OpenOrchestraBaseApiExtension extends Extension
         }
 
         $container->setParameter('open_orchestra_api.controller.http_exception_controller', $config['http_exception_controller']);
+        $container->setParameter('open_orchestra_api.token.expiration_time', $config['token_expiration_time']);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
