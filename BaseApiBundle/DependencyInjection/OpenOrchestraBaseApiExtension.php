bc62296334fd59b54ecada817504146c35353e29
itkg-ngilain <noel.gilain@businessdecision.com>
2015-06-15 19:02:27 +0200

    Replace deprecated method definition->setFactory*

--- BaseApiBundle/DependencyInjection/OpenOrchestraBaseApiExtension.php
+++ BaseApiBundle/DependencyInjection/OpenOrchestraBaseApiExtension.php
@@ -6,6 +6,8 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\DependencyInjection\Definition;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -33,10 +35,9 @@ class OpenOrchestraBaseApiExtension extends Extension
             if (is_array($content)) {
                 $container->setParameter('open_orchestra_api.document.' . $class . '.class', $content['class']);
                 if (array_key_exists('repository', $content)) {
-                    $container->register('open_orchestra_api.repository.' . $class, $content['repository'])
-                        ->setFactoryService($factoryService)
-                        ->setFactoryMethod('getRepository')
-                        ->addArgument($content['class']);
+                    $definition = new Definition($content['repository'], array($content['class']));
+                    $definition->setFactory(array(new Reference($factoryService), 'getRepository'));
+                    $container->setDefinition('open_orchestra_api.repository.' . $class, $definition);
                 }
             }
         }
