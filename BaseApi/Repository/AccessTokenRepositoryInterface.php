269679a3cbf78017fa1c66b0fc4b7cd70e7694c1
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2015-07-01 17:57:03 +0200

    remove deprecated interface

--- BaseApi/Repository/AccessTokenRepositoryInterface.php
+++ BaseApi/Repository/AccessTokenRepositoryInterface.php
@@ -2,7 +2,6 @@
 
 namespace OpenOrchestra\BaseApi\Repository;
 
-use Symfony\Component\Security\Core\User\UserInterface;
 use OpenOrchestra\BaseApi\Model\ApiClientInterface;
 use OpenOrchestra\BaseApi\Model\TokenInterface;
 
@@ -24,19 +23,4 @@ Interface AccessTokenRepositoryInterface
      * @return TokenInterface
      */
     public function findOneByCode($token);
-
-    /**
-     * @param TokenInterface $accessToken
-     *
-     * @deprecated use the AccessTokenManager instead, will be removed in 0.2.7
-     */
-    public function save(TokenInterface $accessToken);
-
-    /**
-     * @param ApiClientInterface $client
-     * @param UserInterface      $user
-     *
-     * @deprecated use the AccessTokenManager instead, will be removed in 0.2.7
-     */
-    public function revokeNonUsedAccessToken(ApiClientInterface $client, UserInterface $user = null);
 }
