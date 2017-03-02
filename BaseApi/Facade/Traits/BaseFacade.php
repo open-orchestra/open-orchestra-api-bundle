103fc53dd3b00c5415f7ab74850762f7ac3c5caa
Amaury Lavieille <amaury.lavieille@gmail.com>
2017-03-02 14:14:23 +0100

    remove links transformer (#117)

--- BaseApi/Facade/Traits/BaseFacade.php
+++ BaseApi/Facade/Traits/BaseFacade.php
@@ -21,14 +21,6 @@ trait BaseFacade
     protected $rights = array();
 
     /**
-     * @Serializer\XmlMap(inline=false, entry="link", keyAttribute="location")
-     * @Serializer\Type("array<string,string>")
-     *
-     * @deprecated To be removed in 2.0
-     */
-    protected $links = array();
-
-    /**
      * @param array $rights
      */
     public function setRights($rights)
@@ -51,36 +43,5 @@ trait BaseFacade
     public function getRights()
     {
         return $this->rights;
-    }
-
-    /**
-     * @param array $links
-     *
-     * @deprecated To be removed in 2.0
-     */
-    public function setLinks($links)
-    {
-        $this->links = $links;
-    }
-
-    /**
-     * @param string $name
-     * @param string $link
-     *
-     * @deprecated To be removed in 2.0
-     */
-    public function addLink($name, $link)
-    {
-        $this->links[$name] = $link;
-    }
-
-    /**
-     * @return array
-     *
-     * @deprecated To be removed in 2.0
-     */
-    public function getLinks()
-    {
-        return $this->links;
     }
 }
